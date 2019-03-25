6c21b31b6cd74a6a315c25752c689d86a9ce53a1
jason19970210 <jason19970210@gmail.com>
2019-03-25 23:02:34 +0800

    New

---
+++ 學校專題_無人車/additionalSensors/humid_temp_sensor/humid_temp_sensor.ino
@@ -0,0 +1,35 @@
+// humid & temp sensor
+// download library `SimpleDHT`
+// Sketch > Include Library > Manage Libraries > Filter
+
+// Using `AVR ISP` for Programmer
+
+#include <SimpleDHT.h>
+
+// for DHT11
+//   |-- VCC: 5V > Orange
+//   |-- GND: GND > Yellow
+//   |-- DATA: 2 (Digital) > Green
+int pinDHT11 = 2; // put 
+SimpleDHT11 dht11(pinDHT11);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed with error code: "); Serial.println(err);delay(1000);
+    return;
+  }
+  
+  Serial.print((int)temperature); Serial.print(" 'C, ");
+  Serial.print((int)humidity); Serial.println(" RH");
+  
+  // DHT11 sampling rate is 1HZ.
+  delay(1500);
+}
