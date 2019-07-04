6b8819ab82b2114782ac8f6e1159f91e50c4f871
jason19970210 <jason19970210@gmail.com>
2019-07-04 11:16:27 +0800

    New

--- 107_3暑期課程/1_HelloWorld/1_HelloWorld.ino
+++ 107_3暑期課程/1_HelloWorld/1_HelloWorld.ino
@@ -1,13 +1,31 @@
+// 太陽能板：
+//    - 紅線：A0
+//    - 黑線：GND
+
+
+const int analogIn = A0;
+double Voltage = 0;
+int RawValue= 0;
+
 void setup() {
   
   Serial.begin(9600);
   
+  
 }
 
 void loop() {
-  
-  Serial.println("Hello World"); // Hello World
-  Serial.println('Hello World'); // 27748
+
+  //Serial.println("Hello World"); // Hello World
+  //Serial.println('Hello World'); // 27748
+  RawValue = analogRead(analogIn);
+  Voltage = (RawValue / 1024.0) * 5000; // Gets you mV
+  Serial.print("Raw Value = " ); // shows pre-scaled value
+  Serial.print(RawValue);
+  Serial.print(" mV = "); // shows the voltage measured
+  Serial.println(Voltage,3); // the '3' after voltage allows you to display 3 digits after decimal point
   delay(1000);
-  
+
+  // 室內光：500 mV
+  // 手電筒：約 1000 mV  
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Arduino
 進度表：[drive](https://docs.google.com/spreadsheets/d/1DuRDyum93YY-XcYEfOB-nmoQAES5sz3MXCBG4r0wEsw/edit#gid=0 "Google")
+
+
++ LED 公規：燈頭切邊 >> 負極
+    + 長正短負
